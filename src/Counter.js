97215a144baf2365cb0ced9db35933667d65daec
Sakshi Priya <priyasakshi8989@gmail.com>
2024-04-30 12:51:00 +0530

    Counter App Completed based on Class

---
+++ src/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="counter">
+        <h2>Counter App</h2>
+        <p>Count: {count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
